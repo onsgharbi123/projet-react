9165b7f63c0793873a897505b2f1e34972c775c7
onsgharbi123 <onsgharbi3151@gmail.com>
2022-06-29 12:55:35 -0700

    chouf

--- src/Compte.js
+++ src/Compte.js
@@ -1,8 +1,13 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Style from "./Compte.module.scss";
 import { useNavigate } from "react-router-dom";
+import { UserContext } from "./Usercontext";
 export default function Compte() {
   const navigate = useNavigate();
+  const { users, setusers } = useContext(UserContext);
+  const [input, setinput] = useState("");
+  const [psd, setpsd] = useState("");
+  const ajout = () => setusers([...users, { name: input, mdp: psd }]);
   return (
     <div className={Style?.container}>
       <div className={Style?.minbox}>
@@ -10,22 +15,24 @@ export default function Compte() {
           Inscrivez-vous pour trouver votre voie avec STAGY
         </h1>
         <div className={Style?.box}>
-          <label>Nom :</label>
+          <label className={Style?.back}>Nom :</label>
           <input type="text" required />
-          <label>Prénom :</label>
+          <label className={Style?.back}>Prénom :</label>
           <input type="text" />
-          <label>Ecole ou Université :</label>
+          <label className={Style?.back}>Ecole ou Université :</label>
           <input type="text" />
-          <label>Année d'obtention du diplôme (effective ou estimée) :</label>
+          <label className={Style?.back}>
+            Année d'obtention du diplôme (effective ou estimée) :
+          </label>
           <input type="text" />
-          <label>Email :</label>
+          <label className={Style?.back}>Email :</label>
           <input type="email" />
-          <label>Mot de passe :</label>
+          <label className={Style?.back}>Mot de passe :</label>
           <input type="password" />
-          <label>Confirmation du mot de passe :</label>
-          <input type="text" />
+          <label className={Style?.back}>Confirmation du mot de passe :</label>
+          <input type="password" />
           <button className={Style?.btn1}>M'inscrire</button>
-          <h4>Vous êtes déjà inscrit.e ?</h4>
+          <h4 className={Style?.back}>Vous êtes déjà inscrit.e ?</h4>
           <button className={Style?.btn2} onClick={() => navigate("/Connect")}>
             Connectez-vous
           </button>
